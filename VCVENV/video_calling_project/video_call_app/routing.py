25324161b90557893c2da05b0dc6b6c5f15a1b8c
m-ally8 <143876631+m-ally8@users.noreply.github.com>
2024-01-04 00:03:45 +0300

    Updated routing.py file to include the consumer.

--- VCVENV/video_calling_project/video_call_app/routing.py
+++ VCVENV/video_calling_project/video_call_app/routing.py
@@ -1,12 +1,14 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
+from .consumers import VideoCallConsumer
 
 application = ProtocolTypeRouter(
     {
         'websocket': AuthMiddlewareStack(
             URLRouter(
                 #Add Websocket URL routing here
+                path('ws/video_call/', VideoCallConsumer.as_asgi()),
             )
 
         ),
