625de42f1f4daa12aff08d72f4e3928d6bd1bd2f
m-ally8 <143876631+m-ally8@users.noreply.github.com>
2024-01-04 00:30:56 +0300

    Updated views.py in the video_call_app app

--- VCVENV/video_calling_project/video_call_app/views.py
+++ VCVENV/video_calling_project/video_call_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def video_call(request):
+    return render(request, 'video_call.html')
+
